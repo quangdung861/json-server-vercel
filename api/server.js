f5c007ebc73170884d05b2e15504af6b48d2581d
quangdung861 <quangdung861@gmail.com>
2023-04-22 17:14:34 +0700

    Update

--- api/server.js
+++ api/server.js
@@ -15,26 +15,9 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 
-server.use(jsonServer.bodyParser);
-
-server.use((req, res, next) => {
-    if (req.method === 'POST') { 
-      req.body.createdAt = moment().valueOf();
-      req.body.updatedAt = moment().valueOf();
-    }
-  
-    if (req.method === 'PUT') {
-      req.method = 'PATCH';
-    }
-  
-    if (req.method === 'PATCH') {
-      req.body.updatedAt = moment().valueOf();
-    }
-  
-    next()
-  })
 
 server.use(auth);
+
 server.use(router)
 server.listen(4000, () => {
     console.log('JSON Server is running')
