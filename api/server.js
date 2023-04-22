bb10bc6d02834508efb1577a71a3fef0546cf4a1
quangdung861 <quangdung861@gmail.com>
2023-04-22 16:42:09 +0700

    Update file: server.js

--- api/server.js
+++ api/server.js
@@ -11,9 +11,34 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(4000, () => {
     console.log('JSON Server is running')
 })
 
 // Export the Server API
 module.exports = server
+
+
+const auth = require('json-server-auth');
+const moment = require('moment');
+
+// server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === 'POST') { 
+    req.body.createdAt = moment().valueOf();
+    req.body.updatedAt = moment().valueOf();
+  }
+
+  if (req.method === 'PUT') {
+    req.method = 'PATCH';
+  }
+
+  if (req.method === 'PATCH') {
+    req.body.updatedAt = moment().valueOf();
+  }
+
+  next()
+})
+
+server.use(auth);
