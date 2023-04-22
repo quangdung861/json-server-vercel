900d09a567b50bbea2bf9a7bbb041383ed704a5b
quangdung861 <quangdung861@gmail.com>
2023-04-22 17:11:17 +0700

    Update

--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+
+const auth = require('json-server-auth');
+const moment = require('moment');
+
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
@@ -11,6 +15,26 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+    if (req.method === 'POST') { 
+      req.body.createdAt = moment().valueOf();
+      req.body.updatedAt = moment().valueOf();
+    }
+  
+    if (req.method === 'PUT') {
+      req.method = 'PATCH';
+    }
+  
+    if (req.method === 'PATCH') {
+      req.body.updatedAt = moment().valueOf();
+    }
+  
+    next()
+  })
+
+server.use(auth);
 server.use(router)
 server.listen(4000, () => {
     console.log('JSON Server is running')
@@ -20,26 +44,8 @@ server.listen(4000, () => {
 module.exports = server
 
 
-const auth = require('json-server-auth');
-const moment = require('moment');
 
-server.use(jsonServer.bodyParser);
 
-server.use((req, res, next) => {
-  if (req.method === 'POST') { 
-    req.body.createdAt = moment().valueOf();
-    req.body.updatedAt = moment().valueOf();
-  }
 
-  if (req.method === 'PUT') {
-    req.method = 'PATCH';
-  }
 
-  if (req.method === 'PATCH') {
-    req.body.updatedAt = moment().valueOf();
-  }
 
-  next()
-})
-
-server.use(auth);
