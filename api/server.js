6edc613c8d1cf8510277bb58d3baab4ec2d32ad9
quangdung861 <quangdung861@gmail.com>
2023-04-22 17:20:22 +0700

    Update

--- api/server.js
+++ api/server.js
@@ -4,18 +4,12 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-
-const auth = require('json-server-auth');
-
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
-
-server.use(auth);
 
 server.use(router)
 server.listen(4000, () => {
