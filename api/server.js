2cde1b505587996345cb5c67bdb81b985cd24582
quangdung861 <quangdung861@gmail.com>
2023-04-23 12:42:06 +0700

    update

--- api/server.js
+++ api/server.js
@@ -11,9 +11,31 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 
-app.use(cors({
-    origin: "*",
-}));
+// app.use(cors({
+//     origin: "*",
+// }));
+
+app.use(express.bodyParser());
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
