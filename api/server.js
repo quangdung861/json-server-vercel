d2779194eef2ea34637dba335710df980eb494c8
quangdung861 <quangdung861@gmail.com>
2023-04-23 10:19:35 +0700

    update

--- api/server.js
+++ api/server.js
@@ -1,66 +1,74 @@
-
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults() 
-const express = require("express")
-const app = express()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const express = require("express");
+const app = express();
 
-const auth = require('json-server-auth');
-const moment = require('moment');
+const auth = require("json-server-auth");
+const moment = require("moment");
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
-server.use(middlewares)
+server.use(middlewares);
 server.use(jsonServer.bodyParser);
 // Add this before server.use(router)
 
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 
 server.use((req, res, next) => {
-    if (req.method === 'POST') { 
-      req.body.createdAt = moment().valueOf();
-      req.body.updatedAt = moment().valueOf();
-    }
-  
-    if (req.method === 'PUT') {
-      req.method = 'PATCH';
-    }
-  
-    if (req.method === 'PATCH') {
-      req.body.updatedAt = moment().valueOf();
-    }
-  
-    next()
-  })
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "POST") {
+    req.body.createdAt = moment().valueOf();
+    req.body.updatedAt = moment().valueOf();
+  }
 
-server.use(router)
+  if (req.method === "PUT") {
+    req.method = "PATCH";
+  }
+
+  if (req.method === "PATCH") {
+    req.body.updatedAt = moment().valueOf();
+  }
+
+  next();
+});
+
+server.use(router);
 server.use(auth);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
